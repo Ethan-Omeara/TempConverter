6f866f2c4d0f1c5b91db853b285d609c5a221e7c
EO4537 <ethan.omeara@hotmail.com>
2024-05-28 10:34:39 +1200

    Save temperature history

--- Temp_Converter.py
+++ Temp_Converter.py
@@ -1,7 +1,12 @@
 from tkinter import *
+import json
+db = './temperature.json'
 
 root = Tk()
 root.title("Temperature Converter")
+
+with open(db, 'r') as file:
+    temp_history = json.loads(file.read())
 
 dispC = StringVar(name="C")
 dispF = StringVar(name="F")
@@ -12,8 +17,7 @@ def updateTemp(var, index, mode):
     if ignoreCommand:
         ignoreCommand = False
         return
-    else:
-        ignoreCommand = True
+    ignoreCommand = True
     
     if var == "C":
         try:
@@ -29,8 +33,14 @@ def updateTemp(var, index, mode):
         except:
             print("Invalid Fahrenheit, setting blank")
             dispC.set("")
-    print(var)
 
+def save_temp():
+    c = round(float(dispC.get()), 2)
+    f = round(float(dispF.get()), 2)
+
+    temp_history.append([c, f])
+    with open(db, 'w') as file:
+        file.write(json.dumps(temp_history))
 
 dispC.trace_add("write", updateTemp)
 dispF.trace_add("write", updateTemp)
@@ -40,5 +50,7 @@ Label(root, text="Fahrenheit:", width=20).grid(row=0, column=1)
 
 Entry(root, textvariable=dispC).grid(row=1, column=0)
 Entry(root, textvariable=dispF).grid(row=1, column=1)
+
+Button(root, text="Save", width=10, command=save_temp).grid(row=2, column=0)
 
 root.mainloop()
